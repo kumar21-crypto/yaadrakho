06f6a4dbb0e9e922f160feb792f8153c3ae04c5f
kumar21-crypto <anuruddhkumar3456@gmail.com>
2025-09-04 17:03:48 +0530

    web icon updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -78,7 +78,8 @@ const Dashboard = () => {
   return (
      <div className="max-w-6xl mx-auto p-4">
       <header className="flex items-center gap-3 mb-4">
-        <h1 className="text-3xl font-bold flex-1">My Notes</h1>
+        <img src="https://cdn-icons-png.flaticon.com/512/9227/9227549.png" alt="Logo" className="h-10 w-10 rounded" /> {/* <-- Add your image here */}
+        <h1 className="text-3xl font-bold flex-1">Yaadrakho</h1>
         <h3 className="text-xl font-bold text-blue-800">{"hii " + user?.name}</h3>
         <input value={q} onChange={e=>setQ(e.target.value)} placeholder="Search…" className="input w-64" />
         <div className="relative">
